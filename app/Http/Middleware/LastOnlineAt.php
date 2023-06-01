7f6533fd33796275a76798d9465b5c40bda4c1f9
Алексей <a_vanyukov@bk.ru>
2023-06-01 21:58:04 +0500

    fix

--- app/Http/Middleware/LastOnlineAt.php
+++ app/Http/Middleware/LastOnlineAt.php
@@ -22,12 +22,12 @@ class LastOnlineAt
 
         $user = auth()->user();
 
-        if ($user && $user->last_online_at->diffInHours(now()) !== 0)
-        {
-            DB::table("users")
-                ->where("id", auth()->user()->id)
-                ->update(["last_online_at" => now()]);
-        }
+//        if ($user && $user->last_online_at->diffInHours(now()) !== 0)
+//        {
+//            DB::table("users")
+//                ->where("id", auth()->user()->id)
+//                ->update(["last_online_at" => now()]);
+//        }
 
         return $next($request);
     }
