15426133c0366977e80267470bf46dd0f7cf81cd
Alexey Vanyukov <a_vanyukov@bk.ru>
2025-02-02 15:08:01 +0500

    update composer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="mt-auto h-20 bg-main">
     <div class="mx-auto h-full max-w-7xl">
         <div class="flex h-full flex-col items-center justify-between text-4xl sm:flex-row">
-            <p>2023</p>
+            <p>2025</p>
             <p>ToasterClub</p>
         </div>
     </div>
