ff8913aaaf7b306a7332b55c48a455587f2f142d
Alexey Vanyukov <a_vanyukov@bk.ru>
2025-02-02 15:00:45 +0500

    fix tailwind and update npm

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,16 @@ module.exports = {
         "./resources/**/*.vue",
     ],
   theme: {
-    colors: {
-        'layout' : '#F3F3F3',
-        'main': '#FAAE18'
+    extend: {
+        colors: {
+            'layout' : '#F3F3F3',
+            'main': '#FAAE18',
+        },
+        boxShadow: {
+            'header': '0px 4px 4px rgba(0, 0, 0, 0.25)',
+            'nav': '0px -4px 19px -3px rgba(0, 0, 0, 0.6)',
+        },
     },
-    boxShadow: {
-        'header': '0px 4px 4px rgba(0, 0, 0, 0.25)',
-        'nav': '0px -4px 19px -3px rgba(0, 0, 0, 0.6)',
-
-
-    },
-    extend: {},
   },
   plugins: [],
 }
